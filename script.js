1ea90bdc39ece5b2187d1c7018c47924e9e05e76
sulaiman1595 <139083473+sulaiman1595@users.noreply.github.com>
2024-03-27 16:57:55 -0400

    script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function selectOption(option) {
             displayCatHeart(); // Display the cat-heart.gif
         });
     } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        // Change text on the "No" button to "You sure about that?"
+        document.getElementById('no-button').innerText = 'You sure about that?'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
